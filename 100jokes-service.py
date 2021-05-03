d8c2b9c2b1907c296addde4491d0c88bfd004be5
Your Name <you@example.com>
2021-05-03 21:20:43 +0200

    expanded service parsing to 100 jokes
    added feture - 20 jokes
    
    expanded service parsing to 100 jokes

--- 100jokes-service.py
+++ 100jokes-service.py
@@ -1,5 +1,8 @@
-from flask import Flask
+# - *- coding: utf- 8 - *-
+from flask import Flask, jsonify
 from flask_restplus import Resource, Api
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +11,19 @@ api = Api(app)
 class JokesTop100(Resource):
     def get(self):
         jokes = {}
-        number = 1
+        number = 1 # jokes counter
+        page_num = list(range(1, 6)) # pg nr - 20 jokes per page
+
+        for page in page_num:
+            url = "http://bash.org.pl/latest/?page=" + str(page)
+            req = requests.get(url)
+            page_content = BeautifulSoup(req.content.decode(req.apparent_encoding), "html.parser")
+
+            for joke in page_content.find_all("div", class_="quote post-content post-body"):
+                jokes[number] = joke.text
+                jokes[number] = jokes[number].replace('\n', '').replace('\t', '').replace('\r', '')
+                number += 1
+        return jsonify(jokes)
 
 @api.route('/health', methods=['GET'])
 class CheckHealth(Resource):
